e9b0be01c950e7c56f4718b371826a08878f5db9
Aysha <sumij19@gmail.com>
2025-02-14 10:29:03 +0530

    Login tests story

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -17,6 +17,8 @@ public class HomePage extends BasePage{
 	//Locators for Register
 	@FindBy(xpath="//a[@title='My Account']") WebElement lnkMyAccount;
 	@FindBy(xpath="//a[text()='Register']") WebElement lnkRegister;
+	@FindBy(linkText = "Login") WebElement lnkLogin;
+	
 	
 	//Action Methods
 	public void clickMyAccount() {
@@ -25,5 +27,9 @@ public class HomePage extends BasePage{
 
 	public void clickRegister() {
 		lnkRegister.click();
+	}
+	
+	public void clickLogin() {
+		lnkLogin.click();
 	}
 }
